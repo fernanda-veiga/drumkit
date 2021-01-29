3310dab87b9fb5676e9cc184f06faf4127849d13
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-29 16:13:34 -0300

    Put each <source> element inside an <audio> element. Created an event listener that plays the sound when the button is clicked.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const audioFiles = document.querySelectorAll(".audio-file");
+const audioFiles = document.querySelectorAll("audio");
 const buttons = document.querySelectorAll(".button");
+
+//Play sound when the button is clicked
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        audioFiles[i].play();
+    });
+}
