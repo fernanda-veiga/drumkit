179f861a1e44f833de120621b662a6e701b9e7d2
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-29 19:15:26 -0300

    Added an element to replay the audio when the user cicks again.

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 //Play sound when the button is clicked
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
+        audioFiles[i].currentTime = 0;
         audioFiles[i].play();
+        /*buttons[i].classList.add("clicked");
+        buttons[i].classList.remove("clicked");*/
         return;
     });
 }
@@ -14,8 +17,11 @@ for (let i = 0; i < buttons.length; i++) {
 window.addEventListener("keydown", function(e){
     for (let i = 0; i < keys.length; i++) {
         if (e.key == keys[i]) {
+            audioFiles[i].currentTime = 0;
             audioFiles[i].play();
+            /*buttons[i].classList.add("clicked");
+            buttons[i].classList.remove("clicked");*/
         }
     }
     return;
-})
+});
