35ff1eb51f41f9eddb034d75ecb1cd07308b23f5
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-29 16:43:29 -0300

    Added a function that plays a sound whenever the corresponding key is pressed.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const audioFiles = document.querySelectorAll("audio");
 const buttons = document.querySelectorAll(".button");
+let keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 
 //Play sound when the button is clicked
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
         audioFiles[i].play();
+        return;
     });
 }
+
+//Play sound when key is pressed
+window.addEventListener("keydown", function(e){
+    for (let i = 0; i < keys.length; i++) {
+        if (e.key == keys[i]) {
+            audioFiles[i].play();
+        }
+    }
+    return;
+})
