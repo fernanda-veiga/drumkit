4d2eb456cdf7098477410445330b1648e3d9abc6
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-29 20:40:41 -0300

    Changed the for loop to a .forEach in the part of the code where it play the sound when the button is clicked.

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 const audioFiles = document.querySelectorAll("audio");
-const buttons = document.querySelectorAll(".button");
+const buttonsNodeList = document.querySelectorAll(".button");
+let buttons = Array.from(buttonsNodeList);
 let keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 
-//Play sound when the button is clicked
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){
-        audioFiles[i].currentTime = 0;
-        audioFiles[i].play();
-        /*buttons[i].classList.add("clicked");
-        buttons[i].classList.remove("clicked");*/
+//Adds an event listener when the transition ends
+/*for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("transitionend", function(){
+        if(e.propertyName !== "transform") {
+            return;
+        }
+        this.classList.remove("clicked");
         return;
     });
+}*/
+
+//Play sound when the button is clicked
+buttons.forEach(item => {
+    item.addEventListener("click", function(){
+        playSound(item);
+    })
+})
+
+function playSound(item) {
+    let index = buttons.indexOf(item);
+    audioFiles[index].currentTime = 0;
+    audioFiles[index].play();
+    return;
 }
+    /*
+        /*
+    })
+})*/
+    
+/*for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){playSound(i)}/*function(){
+        audioFiles[i].currentTime = 0;
+        audioFiles[i].play();
+        buttons[i].classList.add("clicked");
+        buttons[i].classList.remove("clicked");
+        return;
+    });
+}*/
 
 //Play sound when key is pressed
 window.addEventListener("keydown", function(e){
@@ -19,9 +48,15 @@ window.addEventListener("keydown", function(e){
         if (e.key == keys[i]) {
             audioFiles[i].currentTime = 0;
             audioFiles[i].play();
-            /*buttons[i].classList.add("clicked");
-            buttons[i].classList.remove("clicked");*/
+            buttons[i].classList.add("clicked");
+            /*buttons[i].classList.remove("clicked");*/
         }
     }
     return;
 });
+
+//FUNCTIONS
+/*function playSound(i) {
+    audioFiles[i].currentTime = 0;
+    audioFiles[i].play();
+}*/
