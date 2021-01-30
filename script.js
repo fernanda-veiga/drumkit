79d40a2c054d66d5c9ed8778bb5515e29c571ef6
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-29 21:08:37 -0300

    Created a function that gives the button a class when the sound is played.

--- script.js
+++ script.js
@@ -3,55 +3,32 @@ const buttonsNodeList = document.querySelectorAll(".button");
 let buttons = Array.from(buttonsNodeList);
 let keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 
-//Adds an event listener when the transition ends
-/*for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("transitionend", function(){
-        if(e.propertyName !== "transform") {
-            return;
-        }
-        this.classList.remove("clicked");
-        return;
-    });
-}*/
-
-//Play sound when the button is clicked
+//Plays sound when the button is clicked
 buttons.forEach(item => {
     item.addEventListener("click", function(){
         playSound(buttons, item);
     })
 })
 
-//Play sound when key is pressed
+//Plays sound when key is pressed
 window.addEventListener("keydown", function(e){
     playSound(keys, e.key);
 });
 
-//FUNCTION
+//FUNCTIONS
 function playSound(array, item) {
     let i = array.indexOf(item);
+
+    if (i < 0) {return;}//Checks if the element exists in the array
+    
     audioFiles[i].currentTime = 0;
     audioFiles[i].play();
+
+    addClass(i);
     return;
 }
-    /*
-        /*
-    })
-})*/
-    
-/*for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){playSound(i)}/*function(){
-        audioFiles[i].currentTime = 0;
-        audioFiles[i].play();
-        buttons[i].classList.add("clicked");
-        buttons[i].classList.remove("clicked");
-        return;
-    });
-}*/
 
-
-
-//FUNCTIONS
-/*function playSound(i) {
-    audioFiles[i].currentTime = 0;
-    audioFiles[i].play();
-}*/
+function addClass(i) {
+    buttons[i].classList.add("clicked");
+    return;
+}
